b5e59364c7c0e48afe9d117d0d5a8ba556512300
Satyaprakash Singh <86126911+Satyam1Dev@users.noreply.github.com>
2022-06-06 11:33:35 +0530

    Add files via upload

---
+++ booking_ticket/scripts/index.js
@@ -0,0 +1,19 @@
+
+
+let y=document.getElementById("add_to_wallet").addEventListener("click",addmoney)
+
+
+let add=JSON.parse(localStorage.getItem("amount"))
+
+let wallet=document.getElementById("wallet")
+wallet.innerText=add;
+
+function addmoney(){
+    
+    let money=document.getElementById("amount").value
+    let x=add + Number(money)
+    wallet.innerText=x;
+localStorage.setItem("amount",x)
+window.location.reload();
+
+}
